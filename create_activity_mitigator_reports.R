dd1207cdad228d293e1d4f904cdad7c223ee6ebd
Mohammed Ozayr (MLCSU) <Ozayr.Mohammed@mlcsu.nhs.uk>
2024-05-22 15:45:21 +0100

    adds draft prefix to report filenames

--- create_activity_mitigator_reports.R
+++ create_activity_mitigator_reports.R
@@ -5,5 +5,5 @@ purrr::walk(all_schemes,
             ~quarto::quarto_render(
               input = "activity_mitigators_template.qmd",
               execute_params = list("selected_peer" = .x),
-              output_file = glue::glue("{.x}_activity_mitigators_report.html")
+              output_file = glue::glue("DRAFT_{.x}_activity_mitigators_report.html")
             ))
