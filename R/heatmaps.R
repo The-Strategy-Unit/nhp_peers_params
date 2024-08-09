b6f02577c293d885230df858ac2f0d79d569e4d4
Matt Dray <18232097+matt-dray@users.noreply.github.com>
2024-08-09 14:23:42 +0100

    Remove unneeded functinos, restyle

--- R/heatmaps.R
+++ R/heatmaps.R
@@ -1,229 +1,75 @@
-generate_binary_table <- function(
-    activity_mitigators,
-    highlighted_scheme,
-    all_schemes
-) {
+generate_binary_table_all <- function(activity_mitigators, all_schemes) {
   
-  prepare_binary_table(activity_mitigators, highlighted_scheme, all_schemes) |>
-    dplyr::rename("Your Scheme" = highlighted_scheme) |>
-    dplyr::select(-parameter) |> 
-    gt::gt(groupname_col = "activity_type") |>
-    gt::fmt_markdown(columns = tidyselect::contains("Scheme")) |>
-    gt::tab_style_body(
-      style = gt::cell_fill(color = "gold"),
-      values = "&#10003;"
-    ) |>
-    gt::tab_style_body(
-      style = gt::cell_fill(color = "grey"),
-      values = "&#10005;"
-    ) |>
-    gt::cols_align(align = "center", columns = tidyselect::contains("Scheme")) |>
-    gt::grand_summary_rows(
-      columns = -c(activity_type, strategy, perc_peers_picked),
-      fns = perc_params_picked ~ scales::label_percent()
-      (sum(stringr::str_detect("&#10003;", .x)) / length(.x)),
-      side = "top",
-    ) |>
-    gt::cols_hide(1)
-  
-}
-
-prepare_binary_table <- function(
-    activity_mitigators,
-    highlighted_scheme,
-    all_schemes
-) {
-  
-  activity_mitigators |>
-    dplyr::select(-value_1, -value_2) |>
-    dplyr::mutate(flag = 1) |>
-    tidyr::pivot_wider(names_from = "peer", values_from = "flag") |>
-    dplyr::mutate(perc_peers_picked = scales::label_percent()(
-      rowSums(
-        dplyr::across(c(
-          tidyselect::everything(),
-          -activity_type,
-          -strategy,
-          -parameter
-        )),
-        na.rm = TRUE
-      ) /
-        length(dplyr::across(
-          c(
-            tidyselect::everything(),
-            -activity_type,
-            -strategy,
-            -parameter
-          )
-        ))
-    )) |>
-    dplyr::mutate(dplyr::across(
-      c(
-        tidyselect::everything(),
-        -activity_type,
-        -strategy,
-        -parameter,
-        -perc_peers_picked
-      ),
-      ~ ifelse(
-        is.na(.x),
-        htmltools::HTML("&#10005;"), # cross
-        htmltools::HTML("&#10003;") # tick
-      )
-    )) |>
-    dplyr::rename_with(
-      ~ paste("Scheme", c(1:(length(all_schemes) - 1))),
-      (c(
-        tidyselect::everything(),
-        -activity_type,
-        -strategy,
-        -parameter,
-        -perc_peers_picked,
-        -highlighted_scheme
-      ))
-    ) |>
-    dplyr::select(
-      activity_type,
-      strategy,
-      parameter,
-      highlighted_scheme,
-      tidyselect::everything()
-    )
-  
-}
-
-generate_certainty_table <- function(
-    activity_mitigators,
-    highlighted_scheme,
-    all_schemes
-) {
-  
-  certainty_data <- 
-    prepare_certainty_table(activity_mitigators, highlighted_scheme, all_schemes) |> 
-    dplyr::rename("Your Scheme" = highlighted_scheme) |>
+  binary_data <- 
+    prepare_binary_table_all(activity_mitigators, all_schemes) |>
     dplyr::select(-parameter)
   
-  lowest_certainty <- certainty_data |> 
-    dplyr::select(tidyselect::contains("Scheme")) |> 
-    max(na.rm = TRUE) 
-  
-  certainty_data |> 
-    gt::gt(groupname_col = "activity_type") |>
-    gt::data_color(
-      columns = tidyselect::contains("Scheme"),
-      palette = c("grey90","red"),
-      na_color = "white",
-      domain = c(0, lowest_certainty)
-    ) |> 
-    gt::sub_missing(missing_text = htmltools::HTML("&#10005;")) |>
-    gt::tab_style_body(
-      style = gt::cell_text(color = "grey90"),
-      fn = function(x) is.na(x)
-    ) |>
-    gt::cols_align(align = "center", columns = tidyselect::contains("Scheme")) |>
-    gt::tab_stubhead(label = "activity_type")
-  
-}
-
-prepare_certainty_table <- function(
-    activity_mitigators,
-    highlighted_scheme,
-    all_schemes
-) {
-  
-  activity_mitigators |>
-    dplyr::mutate(range_val = value_2 - value_1) |>
-    dplyr::select(-c(value_1, value_2)) |> 
-    tidyr::pivot_wider(names_from = "peer", values_from = "range_val") |>
-    dplyr::rename_with(
-      ~ paste("Scheme", c(1:(length(all_schemes) - 1))),
-      (c(
-        tidyselect::everything(),
-        -activity_type,
-        -strategy,
-        -parameter,
-        -highlighted_scheme
-      ))
-    ) |>
-    dplyr::select(
-      activity_type,
-      strategy,
-      parameter,
-      highlighted_scheme,
-      tidyselect::everything()
-    )
-  
-}
-
-generate_binary_table_all <- function(
-    activity_mitigators,
-    #highlighted_scheme,
-    all_schemes
-) {
-  
-  prepare_binary_table_all(activity_mitigators, all_schemes) |>
-    #dplyr::rename("Your Scheme" = highlighted_scheme) |>
-    dplyr::select(-parameter) |> 
+  binary_data |> 
     gt::gt(groupname_col = "activity_type") |>
     gt::fmt_markdown(columns = tidyselect::any_of(all_schemes)) |>
     gt::tab_style_body(
-      style = list(
-        gt::cell_fill(color = "gold")#,
-        #gt::cell_text(color = "gold")
-        ),
-      values = "&#10003;"
+      style = list(gt::cell_fill(color = "gold")),
+      values = "&#10003;"  # tick
     ) |>
     gt::tab_style_body(
       style = gt::cell_fill(color = "grey"),
-      values = "&#10005;"
+      values = "&#10005;"  # cross
     ) |>
-    gt::cols_align(align = "center", columns = tidyselect::any_of(all_schemes)) |>
+    gt::cols_align(
+      align = "center",
+      columns = tidyselect::any_of(all_schemes)) |>
     gt::grand_summary_rows(
       columns = -c(activity_type, strategy, perc_peers_picked),
-      fns = perc_params_picked ~ scales::label_percent()
-      (sum(stringr::str_detect("&#10003;", .x)) / length(.x)),
+      fns = perc_params_picked ~ scales::label_percent()(
+        sum(stringr::str_detect("&#10003;", .x)) / length(.x)
+      ),
       side = "top",
     ) |>
     gt::cols_hide(1)
   
 }
 
-prepare_binary_table_all <- function(
-    activity_mitigators,
-    #highlighted_scheme,
-    all_schemes
-) {
+prepare_binary_table_all <- function(activity_mitigators, all_schemes) {
   
   activity_mitigators |>
-    dplyr::select(-c(value_1, 
-                     value_2,
-                     baseline_year,
-                     horizon_year,
-                     peer,
-                     scenario,
-                     run_stage
-                     )) |>
+    dplyr::select(
+      -c(
+        value_1, 
+        value_2,
+        baseline_year,
+        horizon_year,
+        peer,
+        scenario,
+        run_stage
+      )
+    ) |>
     dplyr::mutate(flag = 1) |>
     tidyr::pivot_wider(names_from = "peer_year", values_from = "flag") |>
-    dplyr::mutate(perc_peers_picked = scales::label_percent()(
-      rowSums(
-        dplyr::across(c(
-          tidyselect::everything(),
-          -activity_type,
-          -strategy,
-          -parameter
-        )),
-        na.rm = TRUE
-      ) /
-        length(dplyr::across(
-          c(
-            tidyselect::everything(),
-            -activity_type,
-            -strategy,
-            -parameter
+    dplyr::mutate(
+      perc_peers_picked = scales::label_percent()(
+        rowSums(
+          dplyr::across(
+            c(
+              tidyselect::everything(),
+              -activity_type,
+              -strategy,
+              -parameter
+            )
+          ),
+          na.rm = TRUE
+        ) /
+          length(
+            dplyr::across(
+              c(
+                tidyselect::everything(),
+                -activity_type,
+                -strategy,
+                -parameter
+              )
+            )
           )
-        ))
-    )) |>
+      )
+    ) |>
     dplyr::mutate(dplyr::across(
       c(
         tidyselect::everything(),
@@ -238,17 +84,6 @@ prepare_binary_table_all <- function(
         htmltools::HTML("&#10003;") # tick
       )
     )) |>
-    # dplyr::rename_with(
-    #   ~ paste("Scheme", c(1:(length(all_schemes) - 1))),
-    #   (c(
-    #     tidyselect::everything(),
-    #     -activity_type,
-    #     -strategy,
-    #     -parameter,
-    #     -perc_peers_picked,
-    #     #-highlighted_scheme
-    #   ))
-    # ) |>
     dplyr::select(
       activity_type,
       strategy,
@@ -259,24 +94,7 @@ prepare_binary_table_all <- function(
   
 }
 
-midpoint_pal_fun <- function(x){
-  library(scales)
-  lo <- as.numeric(stringr::str_extract(x, "(?<=\\().*(?= - )"))
-  hi <- as.numeric(stringr::str_extract(x, "(?<= - ).*(?=\\))"))
-  
-  range <- hi - lo
-  pal <- scales::col_numeric("Blues",
-                             domain = c(0,1),
-                             reverse = T,
-                             na.color = "white")
-  
-  pal(range)
-}
-
-generate_midpoint_table_all <- function(
-    activity_mitigators,
-    all_schemes
-) {
+generate_midpoint_table_all <- function(activity_mitigators, all_schemes) {
   
   midpoint_data <- 
     prepare_midpoint_table_all(activity_mitigators, all_schemes) |> 
@@ -287,7 +105,7 @@ generate_midpoint_table_all <- function(
     gt::data_color(
       columns = tidyselect::any_of(all_schemes),
       fn = midpoint_pal_fun
-      ) |> 
+    ) |> 
     gt::fmt_markdown() |> 
     gt::sub_missing(missing_text = htmltools::HTML("&#10005;")) |>
     gt::tab_style_body(
@@ -303,39 +121,43 @@ generate_midpoint_table_all <- function(
   
 }
 
-prepare_midpoint_table_all <- function(
-    activity_mitigators,
-    all_schemes
-) {
+prepare_midpoint_table_all <- function(activity_mitigators, all_schemes) {
   
   activity_mitigators |>
-    dplyr::mutate(range_val = value_2 - value_1,
-                  range_label_tmp = ifelse(range_val == 0,
-                                       "Point Estimate",
-                                       paste0(value_1, 
-                                              " - ",
-                                              value_2)),
-                  midpoint = value_1 + ((value_2-value_1)/2),
-                  value_label = paste0('<font size=+1>',
-                                       midpoint, 
-                                       " ",
-                                       "</font>","<br>",
-                                       '<font size=-1>',
-                                       "(",
-                                       range_label_tmp,
-                                       ")",
-                                       "</font>"
-                                       )) |>
-    dplyr::select(-c(value_1, 
-                     value_2,
-                     baseline_year,
-                     horizon_year,
-                     peer,
-                     range_val,
-                     scenario,
-                     run_stage,
-                     midpoint,
-                     range_label_tmp)) |> 
+    dplyr::mutate(
+      range_val = value_2 - value_1,
+      range_label_tmp = dplyr::if_else(
+        range_val == 0,
+        "Point Estimate",
+        paste0(value_1, " - ", value_2)
+      ),
+      midpoint = value_1 + ((value_2 - value_1) / 2),
+      value_label = paste0(
+        '<font size=+1>',
+        midpoint, 
+        " ",
+        "</font>","<br>",
+        '<font size=-1>',
+        "(",
+        range_label_tmp,
+        ")",
+        "</font>"
+      )
+    ) |>
+    dplyr::select(
+      -c(
+        value_1, 
+        value_2,
+        baseline_year,
+        horizon_year,
+        peer,
+        range_val,
+        scenario,
+        run_stage,
+        midpoint,
+        range_label_tmp
+      )
+    ) |> 
     tidyr::pivot_wider(names_from = "peer_year", values_from = "value_label") |>
     dplyr::select(
       activity_type,
@@ -343,7 +165,23 @@ prepare_midpoint_table_all <- function(
       parameter,
       tidyselect::everything()
     )
-   
+  
   
 }
 
+midpoint_pal_fun <- function(x) {
+  
+  lo <- as.numeric(stringr::str_extract(x, "(?<=\\().*(?= - )"))
+  hi <- as.numeric(stringr::str_extract(x, "(?<= - ).*(?=\\))"))
+  range <- hi - lo
+  
+  pal <- scales::col_numeric(
+    "Blues",
+    domain = c(0, 1),
+    reverse = TRUE,
+    na.color = "white"
+  )
+  
+  pal(range)
+  
+}
