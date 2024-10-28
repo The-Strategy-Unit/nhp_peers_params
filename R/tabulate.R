130844eba52d7423d46da05bd5e7a1365a0805dc
Matt Dray <18232097+matt-dray@users.noreply.github.com>
2024-10-28 18:44:28 +0000

    Add function to correct day procedures

--- R/tabulate.R
+++ R/tabulate.R
@@ -21,7 +21,69 @@ extract_params <- function(params) {
         "_", stringr::str_sub(horizon_year, 3, 4)
       )
     ) |> 
-    dplyr::left_join(runs_meta, by = dplyr::join_by("peer" == "dataset"))
+    dplyr::left_join(runs_meta, by = dplyr::join_by("peer" == "dataset")) |> 
+    correct_day_procedures()
+  
+}
+
+correct_day_procedures <- function(x) {
+  
+  # Identify pairs of bads/day_procedures mitigators with flag
+  flagged <- x |>
+    dplyr::mutate(
+      mitigator_code_flag = dplyr::case_when(
+        stringr::str_detect(
+          strategy,
+          "^bads_daycase$|^day_procedures_usually_dc$"  # old name/new name
+        ) ~ "IP-EF-005",  # might as well flag with the mitigator code
+        stringr::str_detect(
+          strategy,
+          "^bads_daycase_occasional$|^day_procedures_occasionally_dc$"
+        ) ~ "IP-EF-006",
+        stringr::str_detect(
+          strategy,
+          "^bads_outpatients$|^day_procedures_usually_op$"
+        ) ~ "IP-EF-007",
+        stringr::str_detect(
+          strategy,
+          "^bads_outpatients_or_daycase$|^day_procedures_occasionally_op$"
+        ) ~ "IP-EF-008",
+        .default = NA_character_
+      )
+    )
+  
+  # Identify where a peer has more than one instance of the code, i.e. the
+  # mitigator is represented by both a bads and a day_procedures version. We'll
+  # use this info to filter out the bads version.
+  dupes <- flagged |>
+    dplyr::count(peer, mitigator_code_flag) |>
+    tidyr::drop_na(mitigator_code_flag) |>
+    dplyr::filter(n > 1)
+  
+  # Remove bads mitigators if there's a day_procedures replacement for it
+  for (i in seq(nrow(dupes))) {
+    flagged <- flagged |>
+      dplyr::filter(
+        !(peer == dupes[[i, "peer"]] &
+            mitigator_code_flag == dupes[[i, "mitigator_code_flag"]] &
+            stringr::str_detect(strategy, "^bads_"))
+      )
+  }
+  
+  # Remaining bads mitigators clearly don't have a replacement day_procedures
+  # version so we can just rename these ones.
+  flagged |>
+    dplyr::mutate(
+      strategy = dplyr::case_match(
+        strategy,
+        "bads_daycase" ~ "day_procedures_usually_dc",
+        "bads_daycase_occasional" ~ "day_procedures_occasionally_dc",
+        "bads_outpatients" ~ "day_procedures_usually_op",
+        "bads_outpatients_or_daycase" ~ "day_procedures_occasionally_op",
+        .default = strategy
+      )
+    ) |>
+    dplyr::select(-mitigator_code_flag)  # remove helper column
   
 }
 
@@ -121,10 +183,3 @@ populate_table <- function(
     dplyr::filter(!is.na(value_1))  # only show rows where mitigator was used
   
 }
-
-
-
-
-
-
-
